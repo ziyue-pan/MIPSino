57105667f0029a922ef9ad7fe557df3a562c356b
Raymond-Ziyue <3180105354@zju.edu.cn>
2020-04-21 17:17:13 +0800

    update: get file path

--- out/extension.js
+++ out/extension.js
@@ -2,9 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
 function activate(context) {
-    console.log('Congratulations, your extension "mipsino" is now active!');
-    let disposable = vscode.commands.registerCommand('mipsino.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello World from MIPSino!');
+    // console.log('Congratulations, your extension "mipsino" is now active!');
+    let disposable = vscode.commands.registerCommand('mipsino.ToHex', () => {
+        var _a;
+        // vscode.window.showInformationMessage('No I am going to!');
+        // const terminal = (<any>vscode.window).createTerminal({name:'MIPSino Terminal'});
+        // terminal.show();
+        var filePath = (_a = vscode.window.activeTextEditor) === null || _a === void 0 ? void 0 : _a.document.fileName;
+        console.log(filePath);
     });
     context.subscriptions.push(disposable);
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,14 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "mipsino" is now active!');
-	let disposable = vscode.commands.registerCommand('mipsino.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from MIPSino!');
+	// console.log('Congratulations, your extension "mipsino" is now active!');
+	let disposable = vscode.commands.registerCommand('mipsino.ToHex', () => {
+		// vscode.window.showInformationMessage('No I am going to!');
+		// const terminal = (<any>vscode.window).createTerminal({name:'MIPSino Terminal'});
+		// terminal.show();
+		var filePath = vscode.window.activeTextEditor?.document.fileName;
+		console.log(filePath);
 	});
 	context.subscriptions.push(disposable);
 }
-export function deactivate() {}
+export function deactivate() { }
